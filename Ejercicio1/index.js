ab8b32dae5e049b4e5a77936bfb4c41f5d02b2c1
friasdavid <friasdavid@gmail.com>
2022-01-27 23:58:37 +0100

    .

---
+++ Ejercicio1/index.js
@@ -0,0 +1,27 @@
+console.log('Hola');
+//base = 16 cm * 23 cm
+//altura = 13 cm
+
+const volumen = 16 * 23 * 13;
+
+console.log('volumen',volumen, 'cm3');
+
+//area = al + 2ab
+
+const arealateral = 23 * 13;
+const aerabase = 16 * 23;
+const areatotal = arealateral + 2 * aerabase;
+console.log('area', areatotal, 'cm2');
+
+//Generalización
+
+const base = 23;
+const altura = 13;
+const ancho = 16;
+
+const volumen2 = base * altura * ancho;
+console.log('volumen2',volumen2, 'cm3');
+
+
+area2 = base * altura + 2 * ancho * base;
+console.log('area2', area2, 'cm2'); 
