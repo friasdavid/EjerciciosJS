2f73f6dbcf957cf9d6ded770ee44d5f8379ba588
friasdavid <friasdavid@gmail.com>
2022-01-31 18:56:08 +0100

    .

---
+++ Ejercicio9/index.js
@@ -0,0 +1,11 @@
+
+const topics = ["JavaScript", "Variables", "funciones", "condicionales", "bucles"];
+
+const modif = topics.map (texto => texto.toUpperCase())
+
+const inverso = modif.reverse ();
+
+console.log("topics= " + topics);
+console.log ("modif= " + modif);
+console.log ("inverso= " + inverso);
+
